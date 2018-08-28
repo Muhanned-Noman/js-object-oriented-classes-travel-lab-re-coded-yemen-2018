ee243e5fe995f7f926f70611da48df46de449813
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-28 18:15:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ class Route {
     this.endingLocation = endingLocation;
   };
   blocksTravelled(){
-
+    let horizontal = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+    let vertical = this.endingLocation.vertical - this.beginningLocation.vertical;
+    return horizontal + vertical;
   };
 };
